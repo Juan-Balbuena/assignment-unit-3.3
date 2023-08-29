5f6578cf405de81c81b7dc3d45099398049a6401
Juan Balbuena <juanbalb48@gmail.com>
2023-08-28 20:24:49 -0500

    I think I added 25 with the correct function. Oncd I did, the auto tests started to fail but everything in the console looks good.

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -24,11 +24,14 @@ let secondItem = supplyChanges[1];
 // 4. The last value in the 'supplyChanges' array was added by mistake.
 //    Remove it from the array and store it inside a new variable called 'removedItem'.
 console.log('4. Remove the last value from supplyChanges:');
-
-
+supplyChanges.pop();
+console.log(supplyChanges);
+let removedItem = 11;
+console.log('The number that was removed was', removedItem);
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Add the value 25 into supplyChanges.');
-
+supplyChanges.push('25');
+console.log(supplyChanges);
 
 // 6. Create three new variables named 'positives', 'negatives', and
 //    'zeroes' whose values are empty arrays. Then, write a for loop
